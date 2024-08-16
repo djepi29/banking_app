916b41aca6d3efd674e635664b795edfc5008c9f
djepi29 <j3anpi3rr3g3org3s@email.com>
2024-08-16 14:47:03 -0400

    implement, auth, appwrite, sentry and set up plaid

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ff2e35c263f919117fdcf1fa9cc013dd@o4507787401756672.ingest.de.sentry.io/4507787443437648",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
